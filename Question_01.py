d5e421b8b7edd65a1c1d8fbe1b289bc610804b36
JHuraibi <fh1328@wayne.edu>
2020-07-03 17:56:01 -0400

    Added required class and methods

---
+++ Question_01.py
@@ -0,0 +1,52 @@
+# Author: Jamal Huraibi, fh1328
+# Assignment 4
+# Question 1
+
+
+class File:
+    def __init__(self):
+    
+    def getNumber():
+        """Returns the file number"""
+        
+    def getName():
+        """Returns the file name."""
+    
+    def setOwner(ownerName):
+        """Adds the name of the file owner."""
+        
+    def getOwner():
+        """Returns the name of the file owner."""
+        
+    def setDate():
+        """Adds the last date and time file was modified."""
+        
+    def getDate():
+        """Returns the last date and time file was modified."""
+        
+    def addLine(aString):
+        """Adds a new line to the end of file."""
+    def deleteLine(aLineNumber):
+        """Deletes a specific line from file."""
+        
+    def getContent():
+        """Fetches the entire content of the file and returns it."""
+        
+    def setContent(aString):
+        """Changes the content of the text file, overwriting any existing text."""
+        
+    def hasWord():
+        """Checks if the file has a specific word in it. Returns true if the word is found, otherwise returns false."""
+    
+    def addFrom():
+        """Adds the content of the other file to the end of the current file."""
+    
+    def countWords():
+        """Counts the number of words in a file and returns it."""
+    
+    def replace(sting1, string2):
+        """Replaces string 1 with string 2 everywhere in the file."""
+
+
+if __name__ == '__main__':
+
