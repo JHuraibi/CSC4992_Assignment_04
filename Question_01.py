e87fc7ec1dd6a02de192c8d134fcbcd4a990e05b
JHuraibi <fh1328@wayne.edu>
2020-07-04 13:23:53 -0400

    Updates and PEP8 method renaming

--- Question_01.py
+++ Question_01.py
@@ -3,50 +3,109 @@
 # Question 1
 
 
-class File:
-    def __init__(self):
+class File():
+    def __init__(self, initial_file_name, initial_content=" "):
+        self.fileNumber = None
+        self.fileName = initial_file_name
+        self.fileOwner = " "
+        self.timeModified = None
+        self.content = initial_content
+        self.__update_file_counter()
+        self.set_date()
     
-    def getNumber():
+    
+    @staticmethod
+    def __update_file_counter():
+        """Updates the file-number counter"""
+    
+    
+    def get_number(self):
         """Returns the file number"""
+        return self.fileNumber
         
-    def getName():
+        
+    def get_name(self):
         """Returns the file name."""
+        return self.fileName
     
-    def setOwner(ownerName):
+    
+    def set_owner(self, owner_name):
         """Adds the name of the file owner."""
+        self.fileOwner = owner_name
         
-    def getOwner():
-        """Returns the name of the file owner."""
         
-    def setDate():
+    def get_owner(self):
+        """Returns the name of the file owner (if one was set). Otherwise, returns an alert that none was set."""
+        if self.fileOwner == " ":
+            return "[No Owner Has Been Set]"
+        else:
+            return self.fileOwner
+        
+        
+    def set_date(self):
         """Adds the last date and time file was modified."""
         
-    def getDate():
+        
+    def get_date(self):
         """Returns the last date and time file was modified."""
         
-    def addLine(aString):
+        
+    def add_line(self, text_to_add):
         """Adds a new line to the end of file."""
-    def deleteLine(aLineNumber):
+        
+        
+    def delete_line(self, line_number):
         """Deletes a specific line from file."""
         
-    def getContent():
+        
+    def get_content(self):
         """Fetches the entire content of the file and returns it."""
         
-    def setContent(aString):
+        
+    def set_content(self, string):
         """Changes the content of the text file, overwriting any existing text."""
         
-    def hasWord():
+        
+    def has_word(self):
         """Checks if the file has a specific word in it. Returns true if the word is found, otherwise returns false."""
     
-    def addFrom():
+    
+    def add_from(self):
         """Adds the content of the other file to the end of the current file."""
     
-    def countWords():
+    
+    def count_words(self):
         """Counts the number of words in a file and returns it."""
     
-    def replace(sting1, string2):
-        """Replaces string 1 with string 2 everywhere in the file."""
+    
+    def replace(self, target, replacement):
+        """Replaces (target: str) with (replacement: str) everywhere in the file."""
+        
+    
+    # TODO: Refactor name to better illustrate purpose
+    @staticmethod
+    def __generate_file_name(name):
+        """Checks if a file already exists with the intended file-name.
+        If no: returns the available file name.
+        If yes: Concatenate a modifier to the file name, re-check with the modified name.
+        """
+        import os
+        file_exists = os.path.exists(name)
+        modifier = 1
+        
+        while file_exists:
+            name = "{}-{}.txt".format(name, modifier)
+            file_exists = os.path.exists(name)
+            modifier = modifier + 1
+        
+        return name
 
 
 if __name__ == '__main__':
+    A = File()
+    B = File()
+    C = File()
 
+    print("A File Number: %" % A.getNumber())
+    print("B File Number: %" % B.getNumber())
+    print("C File Number: %" % C.getNumber())
